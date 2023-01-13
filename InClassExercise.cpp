9bd736515ca0c3e377e1bd2b02209ab78adf1f76
JacobBlock13 <93690106+JacobBlock13@users.noreply.github.com>
2023-01-13 11:48:54 -0700

    Add files via upload

---
+++ InClassExercise.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+	//there are 100 billion stars in the galaxy
+	int stars = 100000000000; //100,000,000,000
+	cout << "Stars in the Galaxy: " << stars << endl;
+	return 0;
+}
+
