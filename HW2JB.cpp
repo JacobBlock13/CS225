0654c98cc9f23d2cbee92f031bb553e0e5d9c0a3
JacobBlock13 <93690106+JacobBlock13@users.noreply.github.com>
2023-02-02 15:22:07 -0700

    Update HW2JB.cpp

--- HW2JB.cpp
+++ HW2JB.cpp
@@ -1,131 +1,52 @@
-//HW4
+//HW2
 //Jacob Block
 
 #include <iostream>
-#include <ctime>
-#include <limits.h>
-//#include <unistd.h>
 using namespace std;
 
-class Car {
-private:
-    string make;
-    double milesPerGallon;
-    double fuelInTank;
-    double fuelTankCapacity;
-    double odometer;
-public:
-    void setCar(string m, double mpg, double fit, double ftc, double o)
-    {
-        make = m;
-        milesPerGallon = mpg;
-        fuelInTank = fit;
-        fuelTankCapacity = ftc;
-        odometer = o;
-    }
-    
-    void showCar()
-    {
-        cout<<endl<<"******************************"<<endl<<"Make: "<<make<<endl<<"Miles Per Gallon: "<<milesPerGallon<<endl<<"Fuel in Tank: "<<fuelInTank<<endl<<"Fuel Tank Capacity: "<<fuelTankCapacity<<endl<<"Odometer reading: "<<odometer<<endl<<"******************************"<<endl;
-    }
-    
-    void fuelUp(int fuel)
-    {
-        if ((fuel + fuelInTank) > fuelTankCapacity) {
-            cout<<"Error: overfueled. Fueled to max capacity instead.";
-            fuelInTank = fuelTankCapacity;
-        }
-        else if (fuel < 0) {
-            cout<<"Error: cannot take fuel out of the car.";
-        }
-        else fuelInTank += fuel;
-    }
-    
-    void drive()
-    {
-        srand(time(0));
-        int randomNumber = rand() % 101;
-        if((fuelInTank - (randomNumber/milesPerGallon)) < fuelTankCapacity) {
-            cout<<"Ran out of gas after"<<milesPerGallon*fuelInTank<<"miles."<<endl;
-            fuelInTank = 0;
-            odometer = milesPerGallon*fuelInTank;
-        }
-        else {
-        fuelInTank = fuelInTank - (randomNumber/milesPerGallon);
-        odometer += randomNumber;
-        }
-    }
-    
-    void drive(int distance)
-    {
-        if(distance < 0) {
-            cout<<"Cannot drive a negative distance sorry.";
-        }
-        else if((fuelInTank - (distance/milesPerGallon)) < fuelTankCapacity) {
-            cout<<"Ran out of gas after "<<milesPerGallon*fuelInTank<<" miles."<<endl;
-            fuelInTank = 0;
-            odometer = milesPerGallon*fuelInTank;
-        }
-        else {
-        odometer += distance;
-        fuelInTank = fuelInTank - (distance/milesPerGallon);
-        }
-    }
+struct Money {
+	double dollars;
+	double cents;
+
 };
 
-int main()
-{
-    Car accord;
-    accord.setCar("Honda", 20, 13.5, 20, 100004);
-    int userInput = 0;
-    int distance = 0;
-    int fuel = 0;
-    accord.showCar();
-    do{
-        cout<<"Select \n1) Go on a joy ride \n2) Drive a certain distance \n3) Fuel up \n4) Exit"<<endl;
-        cin>>userInput;
-        while (!cin) {
-		    cin.clear();
-		    cin.ignore(INT_MAX, '\n');
-		    cout << "Error. Try again: ";
-		    cin >> userInput;
-        }
-        switch(userInput) {
-            case 1:
-                accord.drive();
-                accord.showCar();
-                break;
-            case 2:
-                cout<<"How far would you like to drive?"<<endl;
-                distance = 0;
-                cin>>distance;
-                while (!cin) {
-		            cin.clear();
-		            cin.ignore(INT_MAX, '\n');
-		            cout << "Error. Try again: ";
-		            cin >> distance;
-                }
-                accord.drive(distance);
-                accord.showCar();
-                break;
-            case 3: 
-                cout<<"How much fuel would you like to add?"<<endl;
-                fuel = 0;
-                cin>>fuel;
-                while (!cin) {
-		            cin.clear();
-		            cin.ignore(INT_MAX, '\n');
-		            cout << "Error. Try again: ";
-		            cin >> fuel;
-	            }
-                accord.fuelUp(fuel);
-                accord.showCar();
-                break;
-            case 4:
-                return 1;
-            default:
-                cout<<"Error try again."<<endl;
-        }
-    }
-    while(userInput != 4);
+double getValues(double field) {
+	cin >> field;
+	while (!cin) {
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "Error. Try again: ";
+		cin >> field;
+	}
+	if (field < 0) {
+		field = 1;
+		cout << "Error: negative values are not allowed, setting field to 1." << endl;
+	}
+	return field;
 }
+
+int main() {
+	Money person1 = { 0, 0 };
+	Money person2 = { 0, 0 };
+	Money sum;
+	double person1Total = 0;
+	double person2Total = 0;
+	double sumTotal = 0;
+	cout << "Enter dollars: ";
+	person1.dollars = getValues(person1.dollars);
+	cout << "Enter cents: ";
+	person1.cents = getValues(person1.cents);
+	person1Total = person1.dollars + (person1.cents * .01);
+	printf("You entered: $%0.2f\n", person1Total);
+	cout << "Enter dollars: ";
+	person2.dollars = getValues(person2.dollars);
+	cout << "Enter cents: ";
+	person2.cents = getValues(person2.cents);
+	person2Total = person2.dollars + (person2.cents * .01);
+	printf("You entered: $%0.2f\n", person2Total);
+	sum.dollars = person1.dollars + person2.dollars;
+	sum.cents = person1.cents + person2.cents;
+	sumTotal = sum.dollars + (sum.cents * .01);
+	printf("Sum: $%0.2f\n", sumTotal);
+}
+
